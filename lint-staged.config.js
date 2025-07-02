fe8194d737c8b677d631c518c7204981f7bdc467
Cristiano Santos <cristianorsantos.95@gmail.com>
2025-07-02 15:48:22 +0100

    chore: create script to validate schemas

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
-module.exports = {
+export default {
   '*': 'prettier --write --ignore-unknown',
   '*': 'cspell --no-must-find-files',
+  '*.json': 'node scripts/validate-schemas.js --file {files}',
   'package.json': 'npmPkgJsonLint .',
 };
