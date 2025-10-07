6e00a5a67fbbcda94feb37dd08ff5a99d4dd88be
Andrew Ribeiro <andtankian@gmail.com>
2025-10-07 15:58:53 -0300

    fix: resolve multiple file handling in schema validation lint stage

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 export default {
   '*': ['prettier --write --ignore-unknown', 'cspell --no-must-find-files'],
   '*.{js,ts,mjs,cjs,mts,cts}': 'eslint --fix',
-  '*.json': 'node scripts/validate-schemas.js --file {files}',
+  '*.json': (files) =>
+    `node scripts/validate-schemas.js --file ${files.join(' ')}`,
   'package.json': 'npmPkgJsonLint .',
 };
